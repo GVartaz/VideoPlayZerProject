9ce532c339d36079893f90ad7188afa129189ce9
GVartaz <geoffrey.vartazian@etu.univ-amu.fr>
2020-01-18 15:24:17 +0100

    del https

--- pilote/routes/routes-videos.js
+++ pilote/routes/routes-videos.js
@@ -7,7 +7,7 @@ const { silosConfig } = require('../config/config');
 const videoSilos = silosConfig.silos.videos;
 
 router.post("/logout",function(req,response){
-    axios.post('https://' + videoSilos.host + videoSilos.endpoint + "/logout")
+    axios.post('http://' + videoSilos.host + videoSilos.endpoint + "/logout")
     .then(function(res){
         req.session.destroy();
         response.status(200).json(true);
@@ -18,7 +18,7 @@ router.post("/logout",function(req,response){
 });
 
 router.post("/search/:site", function(req, response){
-    axios.post('https://' + videoSilos.host + videoSilos.endpoint + "/search/"+req.params.site,{
+    axios.post('http://' + videoSilos.host + videoSilos.endpoint + "/search/"+req.params.site,{
         search : req.body.search,
         user : req.session.user,
     }).then(function (res) {
@@ -37,7 +37,7 @@ router.get("/play",function(req,response){
 })
 
 router.post("/addFav/:id/:brand",function(req,response){
-    axios.post('https://' + videoSilos.host + videoSilos.endpoint + "/addFav/"+req.params.id+"/"+req.params.brand,{
+    axios.post('http://' + videoSilos.host + videoSilos.endpoint + "/addFav/"+req.params.id+"/"+req.params.brand,{
         _id : req.session.user._id,
         firstname : req.session.user.firstname,
     })
@@ -50,7 +50,7 @@ router.post("/addFav/:id/:brand",function(req,response){
 })
 
 router.post("/addVideoToPlaylist/:video/:playlist",function(req,response){
-    axios.post('https://' + videoSilos.host + videoSilos.endpoint + "/addVideoToPlaylist/"+req.params.video+"/"+req.params.playlist,{
+    axios.post('http://' + videoSilos.host + videoSilos.endpoint + "/addVideoToPlaylist/"+req.params.video+"/"+req.params.playlist,{
         _id : req.session.user._id,
     })
     .then(function (res) {
@@ -61,7 +61,7 @@ router.post("/addVideoToPlaylist/:video/:playlist",function(req,response){
 })
 
 router.post("/open/:id/:brand",function(req,response){
-    axios.post('https://' + videoSilos.host + videoSilos.endpoint + "/open/"+req.params.id+"/"+req.params.brand)
+    axios.post('http://' + videoSilos.host + videoSilos.endpoint + "/open/"+req.params.id+"/"+req.params.brand)
     .then(function (res) {
         req.session.video = res.data;
         response.status(200).json(true);
@@ -74,7 +74,7 @@ et on appelle youtube avec l'id des videos, on les renvoie */
 router.get("/favorites",function(req,response){
     req.session.video = {};
     if(typeof req.session.user === 'undefined' ){
-        axios.get('https://' + videoSilos.host + videoSilos.endpoint + "/favorites",{
+        axios.get('http://' + videoSilos.host + videoSilos.endpoint + "/favorites",{
         params: {
             _id : null
         }
@@ -84,7 +84,7 @@ router.get("/favorites",function(req,response){
             response.status(500).json(error);
         });
     } else {
-        axios.get('https://' + videoSilos.host + videoSilos.endpoint + "/favorites",{
+        axios.get('http://' + videoSilos.host + videoSilos.endpoint + "/favorites",{
         params: {
             _id : req.session.user._id,
             firstname : req.session.user.firstname,
@@ -99,7 +99,7 @@ router.get("/favorites",function(req,response){
 
 
 router.delete("/deleteFav/:id",function(req,response){
-    axios.delete('https://' + videoSilos.host + videoSilos.endpoint + "/deleteFav/"+req.params.id,{
+    axios.delete('http://' + videoSilos.host + videoSilos.endpoint + "/deleteFav/"+req.params.id,{
         user :{  
             _id : req.session.user._id,
             firstname : req.session.user.firstname,
@@ -114,7 +114,7 @@ router.delete("/deleteFav/:id",function(req,response){
 
 router.get("/getPlaylistSet",function(req,response){
     req.session.playlist = {};
-    axios.get('https://' + videoSilos.host + videoSilos.endpoint + "/getPlaylistSet",{
+    axios.get('http://' + videoSilos.host + videoSilos.endpoint + "/getPlaylistSet",{
         params: {
             _id : req.session.user._id,
             firstname : req.session.user.firstname,
@@ -128,7 +128,7 @@ router.get("/getPlaylistSet",function(req,response){
 })
 
 router.post("/addPlaylist",function(req,response){
-    axios.post('https://' + videoSilos.host + videoSilos.endpoint + "/addPlaylist",{
+    axios.post('http://' + videoSilos.host + videoSilos.endpoint + "/addPlaylist",{
         name : req.body.name,
         _id : req.session.user._id,
     }).then(function (res) {
@@ -140,7 +140,7 @@ router.post("/addPlaylist",function(req,response){
 
 router.post("/openPlaylist/:id",function(req,response){
     req.session.playlist = req.params.id;
-    axios.post('https://' + videoSilos.host + videoSilos.endpoint + "/openPlaylist/"+req.params.id)
+    axios.post('http://' + videoSilos.host + videoSilos.endpoint + "/openPlaylist/"+req.params.id)
     .then(function (res) {
         response.status(200).json(res.data);;
     }).catch(function (error) {
@@ -149,7 +149,7 @@ router.post("/openPlaylist/:id",function(req,response){
 })
 
 router.delete("/deletePlaylist/:id",function(req,response){
-    axios.delete('https://' + videoSilos.host + videoSilos.endpoint + "/deletePlaylist/"+req.params.id).
+    axios.delete('http://' + videoSilos.host + videoSilos.endpoint + "/deletePlaylist/"+req.params.id).
     then(function (res) {
         response.status(200).json(res.data);
     }).catch(function (error) {
@@ -158,7 +158,7 @@ router.delete("/deletePlaylist/:id",function(req,response){
 })
 
 router.delete("/deleteFromPlaylist/:id",function(req,response){
-    axios.delete('https://' + videoSilos.host + videoSilos.endpoint + "/deleteFromPlaylist/"+req.params.id,{
+    axios.delete('http://' + videoSilos.host + videoSilos.endpoint + "/deleteFromPlaylist/"+req.params.id,{
         params: {
             playlist : req.session.playlist,
         },
